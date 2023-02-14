c9338e9305db6244b2a689bd8f1864680993dd6c
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2023-02-14 17:41:22 +0100

    first commit

---
+++ resources/views/pages/private-home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts. main-layout')
+
+@section('content')
+    <h1>Hello World to logged user!</h1>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\MainController;
+
+Route::get('/', [MainController::class, 'home']);
+
+Route::middleware(['auth', 'verified'])
+    ->name('private.')
+    ->prefix('private')
+    ->group(function () {
+        Route::get('/', [MainController::class, 'privateHome']);
+    });
+
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
