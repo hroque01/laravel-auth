31486e63b1d0b4557dda1355c745a02ec04f64d2
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2023-02-14 20:42:39 +0100

    Print on frontend

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts. main-layout')
 
 @section('content')
-    <h1>Hello World!</h1>
+    <h1>Projects</h1>
+
+    <ul>
+        @foreach ($projects as $project)
+        <li>
+            <h2>{{ $project -> name  }}</h2>
+            <img class="pjImg" src="{{ $project -> main_image }}" alt="">
+            <div> {{ $project -> release_date }}</div>
+        </li>
+            
+        @endforeach
+    </ul>
 @endsection
